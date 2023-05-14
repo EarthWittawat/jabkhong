c3bcc8aa60464c5fc52d315cab6c19ea86716472
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-14 21:15:42 +0700

    next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Prompt } from 'next/font/google'
+import { SessionProvider } from 'next-auth/react'
+
 const prompt = Prompt({
   subsets: ['latin'],
   weight: ['400', '700'],
 })
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (<main className={prompt.className}>
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
     <Component {...pageProps} />
-  </main>) 
+  </SessionProvider>
+  ) 
 }
