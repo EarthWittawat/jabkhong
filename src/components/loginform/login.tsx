ed9eb2a41549b026245b5ec02a975fdc9245f6ed
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-05-15 15:18:13 +0700

    Update login.tsx

--- src/components/loginform/login.tsx
+++ src/components/loginform/login.tsx
@@ -16,6 +16,7 @@ export default function Loginform(this: any) {
   };
     return (
       <>
+       <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <div className="flex flex-col items-center ">
       <div className="flex flex-col mb-2">
       <Input onChange={(e)=> {
